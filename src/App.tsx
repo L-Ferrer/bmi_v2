36cd6f1a09aca89133635d8d63a6848da36c826a
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-03-27 00:19:19 +0200

    Added button to submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Typography } from 'antd'
-import { InputNumber } from 'antd';
-import { Radio } from 'antd';
+import { Typography, InputNumber, Radio, Button } from 'antd'
 import './App.css'
 
 const { Title } = Typography
@@ -10,24 +8,37 @@ function App() {
   const [height, setHeight] = useState(0)
   const [weight, setWeight] = useState(0)
   const [gender, setGender] = useState("m")
-  const [bmi, setBMI] = useState(0)
   const [sbmi, setSBMI] = useState("")
   const [status, setStatus] = useState("_____")
-  const [message, setMessage] = useState("Sie sind ein Mann, mit "+status+".")
+  const [message, setMessage] = useState("Sie sind ein Mann, mit " + status + ".")
   const bmi_min = 10
   const bmi_max = 50
 
   useEffect(() => {
-    if(bmi == (undefined || Infinity ||NaN) || bmi < bmi_min || bmi > bmi_max) {
-      setBMI(0)
-    } else setBMI(Math.round((weight / ((height / 100) * (height / 100)))))
-    if(gender == "m"){
-      console.log("m")  
+    setSBMI("0")
+  }, [])
+
+  function calculateBMI() {
+    let result;
+    if (height === 0 || weight === 0) {
+      setSBMI("0")
     }
-    if(bmi == 0){
-      setSBMI("")
+    else {
+      result = (weight / ((height / 100) * (height / 100)))
+      if (typeof result !== "number") {
+        setSBMI("Invalid")
+        return
+      } if (result < bmi_min) {
+        setSBMI("< " + bmi_min.toString())
+        return
+      }
+      if (result > bmi_max) {
+        setSBMI("> " + bmi_max.toString())
+        return
+      }
+      setSBMI(Math.round(result).toString())
     }
-  }, [height, weight, gender])
+  }
 
   return (
     <>
@@ -37,19 +48,19 @@ function App() {
         </div>
         <div className="content_container">
           <div className="content">
-          <div className="content_title">
+            <div className="content_title">
               <Title level={3}></Title>
             </div>
             <div className="content_form">
               <div className="form">
                 <div className="form_item">
                   <label htmlFor="height" className="label">Height in cm</label>
-                  <InputNumber min={1} max={300} value={height} onChange={(e: any) => setHeight(e)} />
+                  <InputNumber min={1} max={300} value={height} onChange={(e: any) => setHeight(e)} onPressEnter={calculateBMI} />
                 </div>
               </div>
               <div className="form_item">
                 <label htmlFor="weight" className="label">Weight in kg</label>
-                <InputNumber min={1} max={300} value={weight} onChange={(e: any) => setWeight(e)} />
+                <InputNumber min={1} max={300} value={weight} onChange={(e: any) => setWeight(e)} onPressEnter={calculateBMI} />
               </div>
               <div className="form_item">
                 <Radio.Group value={gender} onChange={(e: any) => setGender(e.target.value)}>
@@ -57,9 +68,12 @@ function App() {
                   <Radio value="f" className="radio">Female</Radio>
                 </Radio.Group>
               </div>
+              <div className="button">
+                <Button type="primary" onClick={calculateBMI}>Calculate</Button>
+              </div>
 
               <div className="form_item" id="bmi">
-                <label htmlFor="bmi" className="bmi_title">Your BMI</label>
+                <label htmlFor="bmi" className="bmi_title">Your BMI is</label>
                 <p className="label" id="bmi_label">{sbmi}</p>
               </div>
             </div>
