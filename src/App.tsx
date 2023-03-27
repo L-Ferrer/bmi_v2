f6a11b4b7459481bf2376c5e779f9c33f3ebd33c
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-03-27 09:00:58 +0200

    Added age input and text message

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ const { Title } = Typography
 function App() {
   const [height, setHeight] = useState(0)
   const [weight, setWeight] = useState(0)
+  const [age, setAge] = useState(0)
   const [gender, setGender] = useState("m")
   const [sbmi, setSBMI] = useState("")
-  const [status, setStatus] = useState("_____")
-  const [message, setMessage] = useState("Sie sind ein Mann, mit " + status + ".")
+  const [message, setMessage] = useState("")
   const bmi_min = 10
   const bmi_max = 50
 
@@ -19,16 +19,14 @@ function App() {
   }, [])
 
   function calculateBMI() {
+    console.log("Calculating BMI")
     let result;
     if (height === 0 || weight === 0) {
       setSBMI("0")
     }
     else {
       result = (weight / ((height / 100) * (height / 100)))
-      if (typeof result !== "number") {
-        setSBMI("Invalid")
-        return
-      } if (result < bmi_min) {
+      if (result < bmi_min) {
         setSBMI("< " + bmi_min.toString())
         return
       }
@@ -37,6 +35,215 @@ function App() {
         return
       }
       setSBMI(Math.round(result).toString())
+      //* Data from https://plakos-akademie.de/bmi-rechner/
+      // 1-10
+      if (age >= 1 && age <= 10) {
+        if (gender === "m") {
+          if (result < 15) {
+            setMessage("Underweight")
+            return
+          } if (result > 18) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        } else {
+          if (result < 15) {
+            setMessage("Underweight")
+            return
+          } if (result > 19) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        }
+      }
+      // Age 11-18
+      if (age >= 11 && age <= 18) {
+        if (gender === "m") {
+          if (result < 17) {
+            setMessage("Underweight")
+            return
+          } if (result > 22) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        } else {
+          if (result < 17) {
+            setMessage("Underweight")
+            return
+          } if (result > 23) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        }
+      }
+      // Age 19-24
+      if (age >= 19 && age <= 24) {
+        if (gender === "m") {
+          if (result < 19) {
+            setMessage("Underweight")
+            return
+          } if (result > 24) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        } else {
+          if (result < 18) {
+            setMessage("Underweight")
+            return
+          } if (result > 23) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        }
+      }
+      // Age 25-34
+      if (age >= 25 && age <= 34) {
+        if (gender === "m") {
+          if (result < 20) {
+            setMessage("Underweight")
+            return
+          } if (result > 25) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        } else {
+          if (result < 19) {
+            setMessage("Underweight")
+            return
+          } if (result > 24) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        }
+      }
+      // Age 35-44
+      if (age >= 35 && age <= 44) {
+        if (gender === "m") {
+          if (result < 21) {
+            setMessage("Underweight")
+            return
+          } if (result > 26) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        } else {
+          if (result < 20) {
+            setMessage("Underweight")
+            return
+          } if (result > 25) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        }
+      }
+      // Age 45-54
+      if (age >= 45 && age <= 54) {
+        if (gender === "m") {
+          if (result < 22) {
+            setMessage("Underweight")
+            return
+          } if (result > 27) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        } else {
+          if (result < 21) {
+            setMessage("Underweight")
+            return
+          } if (result > 26) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        }
+      }
+      // Age 55-64
+      if (age >= 55 && age <= 64) {
+        if (gender === "m") {
+          if (result < 23) {
+            setMessage("Underweight")
+            return
+          } if (result > 28) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        } else {
+          if (result < 22) {
+            setMessage("Underweight")
+            return
+          } if (result > 27) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        }
+      }
+      // Age 65+
+      if (age >= 65) {
+        if (gender === "m") {
+          if (result < 24) {
+            setMessage("Underweight")
+            return
+          } if (result > 29) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        } else {
+          if (result < 23) {
+            setMessage("Underweight")
+            return
+          } if (result > 28) {
+            setMessage("Overweight")
+            return
+          } else {
+            setMessage("Normal")
+            return
+          }
+        }
+      }
     }
   }
 
@@ -63,6 +270,10 @@ function App() {
                 <InputNumber min={1} max={300} value={weight} onChange={(e: any) => setWeight(e)} onPressEnter={calculateBMI} />
               </div>
               <div className="form_item">
+                <label htmlFor="weight" className="label">Age</label>
+                <InputNumber min={1} max={150} value={age} onChange={(e: any) => setAge(e)} onPressEnter={calculateBMI} />
+              </div>
+              <div className="form_item">
                 <Radio.Group value={gender} onChange={(e: any) => setGender(e.target.value)}>
                   <Radio value="m" className="radio">Male</Radio>
                   <Radio value="f" className="radio">Female</Radio>
@@ -75,6 +286,7 @@ function App() {
               <div className="form_item" id="bmi">
                 <label htmlFor="bmi" className="bmi_title">Your BMI is</label>
                 <p className="label" id="bmi_label">{sbmi}</p>
+                <p className="msg">{message}</p>
               </div>
             </div>
           </div>
